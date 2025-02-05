4953229d48b614fdaa494c1972bd07bf85824858
TNC9 <koonbenzkub@hotmail.com>
2025-02-05 21:37:11 +0700

    gamestate

---
+++ src/test/java/project/kombat/KombatApplicationTests.java
@@ -0,0 +1,13 @@
+package project.kombat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KombatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
